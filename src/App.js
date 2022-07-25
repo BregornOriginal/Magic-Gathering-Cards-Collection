7bb34ce4c68bc5bfc8a81436edb2575d256cbf8c
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-25 15:28:18 -0300

    install react router and delete dead code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Header from './components/header/header';
+import GameDetail from './components/pages/details/gameDetail';
+import MmoGames from './components/pages/home/homePage';
+import Filter from './components/filter/filter';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>React Capstone</h1>
+      <BrowserRouter>
+        <Header />
+        <Filter />
+        <Routes>
+          <Route path="/" element={<MmoGames />} />
+          <Route path="/details" element={<GameDetail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
