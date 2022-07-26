b0aaaa9bf54f41d04f982203992eaa39291a55ce
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-26 20:38:34 -0300

    add provider and fix variable names

---
+++ src/components/pages/details/cardDetail.js
@@ -0,0 +1,7 @@
+export default function CardDetail() {
+  return (
+    <section className="detail-of-card">
+      <h1>Detail card</h1>
+    </section>
+  );
+}
--- src/components/pages/details/gameDetail.js
+++
@@ -1,7 +0,0 @@
-export default function GameDetail() {
-  return (
-    <section className="detail-of-MmoGame">
-      <h1>Detail game</h1>
-    </section>
-  );
-}
