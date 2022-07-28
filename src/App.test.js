e440a8853911668c176d765ef687effca8a58e1e
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-07-28 18:22:20 -0300

    add header test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './components/store/configureStore';
+import { BrowserRouter } from 'react-router-dom';
 
+import Header from './components/header/header';
 import App from './App';
 
 const MockApp = () => (
@@ -16,4 +19,9 @@ describe('Integration Test', () => {
     render(<MockApp />);
     expect(screen.getByText('Collection of Cards Magic The Gatering')).toBeInTheDocument();
   });
+});
+
+test('Header displayed', () => {
+  render(<Header />, { wrapper: BrowserRouter });
+  expect(screen.getByAltText('IMDB Logo')).toBeInTheDocument();
 });
